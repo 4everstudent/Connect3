f85c374831820edc2d68cc6082b5e230a72024c9
4EverStudent <mail@4everstudent.com>
2016-09-15 18:47:05 +0100

    Add GridLayout and board. Add red discs.

---
+++ app/src/main/java/a4everstudent/connect3/MainActivity.java
@@ -0,0 +1,17 @@
+package a4everstudent.connect3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+// TODO: 15-09-2016 add dropdown animation
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
