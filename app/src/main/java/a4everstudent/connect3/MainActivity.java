b342b961a39830b5e5da4eedf86540b5010a4d77
4EverStudent <mail@4everstudent.com>
2016-09-16 10:41:04 +0100

    Add function to check if a player won .

--- app/src/main/java/a4everstudent/connect3/MainActivity.java
+++ app/src/main/java/a4everstudent/connect3/MainActivity.java
@@ -6,34 +6,64 @@ import android.view.View;
 import android.widget.ImageView;
 
 
-
-
+// TODO: 16-09-2016 Fix layout in multiple devices
 
 public class MainActivity extends AppCompatActivity {
 
     //0=yellow, 1=red
     int activePlayer = 0;
+    //2 means not played yet
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+    int [][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8},{0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2, 4, 6}};
+
+
+    private void checkWin(){
+        for (int [] winningPosition: winningPositions){
+            if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                    gameState[winningPosition[1]]== gameState[winningPosition[2]]&&
+                    gameState[winningPosition[0]] != 2){
+                //someone won the game
+                System.out.println(gameState[winningPosition[0]]+" We have a winner!");
+            }
+            else {
+
+            }
+        }
+    }
 
     public void dropIn(View view){
 
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(-1000f);
+        int  tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if(activePlayer == 0){
+        if(gameState[tappedCounter]==2){
 
-            counter.setImageResource(R.drawable.yellow);
+            gameState[tappedCounter] =activePlayer;
 
-            activePlayer = 1;
+            counter.setTranslationY(-1000f);
+
+            if(activePlayer == 0){
+
+                counter.setImageResource(R.drawable.yellow);
+
+                activePlayer = 1;
+            }
+            else{
+
+                counter.setImageResource(R.drawable.red);
+
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(1000);
+
+            checkWin();
+
+
+
         }
-        else{
 
-            counter.setImageResource(R.drawable.red);
-
-            activePlayer = 0;
-        }
-
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(1000);
 
     }
 
